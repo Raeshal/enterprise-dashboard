5388c8dc7929d5d68d2c3dec519451c24d32e19f
Raeshal <raeshalkhurana07@gmail.com>
2026-01-29 23:58:41 +0530

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+   path: '',
+   component: Layout,
+   children: [
+     {
+       path: 'dashboard',
+       loadChildren: () =>
+         import('./features/dashboard/dashboard-routing-module')
+         .then(m => m.DashboardRoutingModule)
+     },
+     { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+   ]
+ }
+];
